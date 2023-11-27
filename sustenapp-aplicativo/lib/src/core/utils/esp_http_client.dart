13578744204a7e71c072e08f51ecde1c99198f2f
Wiliam Melo <109559789+WiliamMelo01@users.noreply.github.com>
2023-11-27 08:34:05 -0300

    Update esp_http_client.dart

--- sustenapp-aplicativo/lib/src/core/utils/esp_http_client.dart
+++ sustenapp-aplicativo/lib/src/core/utils/esp_http_client.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 final BaseOptions options = BaseOptions(
-  baseUrl: 'http://192.168.18.226',
+  baseUrl: 'http://192.168.134.68',
 );
 
 final MyEspHttpClient = Dio(options);
